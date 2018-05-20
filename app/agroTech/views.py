e843284b1321b20c79f2db2df3db0b84ae763b57
ctezna <cteznas@eafit.edu.co>
2018-05-19 21:51:18 -0500

    v0.6

--- app/agroTech/views.py
+++ app/agroTech/views.py
@@ -1,5 +1,6 @@
 from agroTech import app
-import Adafruit_DHT
+#import Adafruit_DHT
+import random
 from flask import render_template, request
 from agroTech.static.data.db import get_db, init_db
 
@@ -8,23 +9,18 @@ app.debug = True
 @app.route('/')
 @app.route('/index.html',methods=('GET','POST'))
 def index():
-    # if request.method == 'POST':
-    #     plantId = request.form['plantId']
-    #     db = init_db()
-    #     db.execute(
-    #             'INSERT INTO pots (plantId) VALUES (?)',
-    #             (plantId)
-    #         )
-    #     db.commit()
-    #     return render_template('index.html',plantId=plantId)
-
-	humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.AM2302, 17)
-	faren = temperature * 9/5.0 + 32
+	#humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.AM2302, 17)
+	humidity = random.uniform(1,101)
+	temperature = random.uniform(20,39)
+	ph = random.uniform(5,8)
 	if humidity is None and temperature is None:
-		return render_template("index.html",temp=-1,hum=-1)
+		return render_template("index.html",temp=-1,hum=-1,ph=ph)
+		if ph is None:
+		    return render_template("index.html",temp=-1,hum=-1,ph=-1)
 	else:
-		return render_template("index.html",temp=temperature,faren=faren,hum=humidity)
-    #return render_template('index.html')
+		if ph is None:
+			return render_template("index.html", temp=temperature, hum=humidity,ph=-1)
+		return render_template("index.html", temp=temperature, hum=humidity,ph=ph)
 
 @app.route('/settings.html')
 def settings():
