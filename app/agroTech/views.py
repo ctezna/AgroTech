df745bc2e1d925a9f410b686f6ddd14747cbb248
ctezna <cteznas@eafit.edu.co>
2018-05-20 12:22:59 -0500

    Added support for Notification handling.
    v0.8

--- app/agroTech/views.py
+++ app/agroTech/views.py
@@ -1,6 +1,9 @@
 from agroTech import app
 #import Adafruit_DHT
 import random
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 from flask import render_template, request
 from agroTech.static.data.db import get_db, init_db
 
@@ -9,6 +12,12 @@ app.debug = True
 @app.route('/')
 @app.route('/index.html',methods=('GET','POST'))
 def index():
+	if request.method == 'POST':
+		email = request.form['email']
+		subject = '- Notification from AgroTech Automations -'
+		msg = 'Please check the status of your SmartPots, we have received an alert that one of your plants requires attention.'
+		sendemail(email,subject,msg)
+
 	#humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.AM2302, 17)
 	humidity = random.uniform(1,1001)
 	temperature = random.uniform(-20,100)
@@ -52,3 +61,19 @@ def newPot():
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('e404.html'), 404
+
+def sendemail(to_email,sub,message):
+	fromaddr = "agrotech.notification@gmail.com"
+	toaddr = to_email
+	msg = MIMEMultipart()
+	msg['From'] = fromaddr
+	msg['To'] = toaddr
+	msg['Subject'] = sub
+	body = message
+	msg.attach(MIMEText(body, 'plain'))
+	server = smtplib.SMTP('smtp.gmail.com', 587)
+	server.starttls()
+	server.login(fromaddr, "smartpot06")
+	text = msg.as_string()
+	server.sendmail(fromaddr, toaddr, text)
+	server.quit()
